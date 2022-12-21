8768e2f75141ca9a6ac6998503f2ea252524c2fa
Ahriashi <113824104+Ahriashi@users.noreply.github.com>
2022-12-21 17:49:54 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import random
+
+logging.basicConfig(filename="ex.log", level=logging.INFO)
+
+
+def play_game(N, k):
+    # загадываем случайное число от 1 до N
+    number = random.randint(1, N)
+    logging.info(f"the computer chose a number {number}")
+
+    for i in range(k):
+        # получаем попытку от пользователя
+        guess = int(input("Введите число от 1 до {}: ".format(N)))
+
+        # проверяем, правильно ли отгадано число
+        if guess == number:
+            logging.info("Player win!")
+            print("Вы угадали!")
+            return
+        elif guess < number:
+            print("Загаданное число больше")
+            logging.info("Player called number {} The hidden number is greater".format(guess))
+        else:
+            print("Загаданное число меньше")
+            logging.info("Player called number {} The hidden number is less".format(guess))
+
+    # если у пользователя закончились попытки
+    logging.info("Attempts ended.")
+
+
+# запрашиваем у пользователя N и k
+N = int(input("Введите число, до которого загадывать: "))
+k = int(input("Введите количество попыток: "))
+
+# запускаем игру
+play_game(N, k)
